793c0364cf2b983333ea4c72aa1ee094d40d567e
lining <liningdejia@163.com>
2019-06-05 11:23:44 +0800

    动态引入模块的提取

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-import _ from 'lodash';
-import printMe from './print.js';
-
-
-
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-    element.innerHTML = _.join(['嘿eee嘿','hello', 'webpack', 'are you ok', 'i am ok 是而ve哈哈ryOK', 'are you ok i am ok'], ' ');
-
-    btn.innerHTML = 'clickMe';
-    btn.onclick = printMe;
-    // dd
-    console.log('are you watching me : npm run watch for a test');
-    element.appendChild(btn);
-    return element;
+function getComponent() {
+    // 如果没有注释 则默认打包出来的是[id].bundle/hash.js  这里边的注释定义了提取打包出来的文件名
+    // 加了注释，打包出来的文件名 vendors~myLodash.6a86543.js
+    return import(/* webpackChunkName:'myLodash'*/'lodash').then(({default:_}) => {
+        var element = document.createElement('div');
+        element.innerHTML = _.join(['hello', 'world'], ' ');
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
 }
-
-document.body.appendChild(component());
-
-if (module.hot) {
-    console.log(`hhhh`);
-    module.hot.accept('./print.js', function() {
-        console.log('Accepting the updated printMe module!');
-        printMe();
-    })
-} else {
-    console.log('我不是hot');
-}
-
+getComponent().then(component => {
+    document.body.appendChild(component);
+})
